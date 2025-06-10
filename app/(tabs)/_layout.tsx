e8154de19ad7753e907dd4faab4d6be4b75c8b6e
WillianHelmuthN <willianhelmuth@gmail.com>
2025-06-10 20:15:11 -0300

    Substitui ícones de tabulação por ícones específicos do AntDesign e MaterialIcons, melhorando a representação visual das telas "Comparar" e "Lista".

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,12 @@
-import { Tabs } from "expo-router";
-import React from "react";
-import { Platform } from "react-native";
-
 import { HapticTab } from "@/components/HapticTab";
-import { IconSymbol } from "@/components/ui/IconSymbol";
 import TabBarBackground from "@/components/ui/TabBarBackground";
 import { Colors } from "@/constants/Colors";
 import { useColorScheme } from "@/hooks/useColorScheme";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { Tabs } from "expo-router";
+import React from "react";
+import { Platform } from "react-native";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -32,7 +32,11 @@ export default function TabLayout() {
         options={{
           title: "Comparar",
           tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="cart.fill" color={color} />
+            <AntDesign
+              name="shoppingcart"
+              size={28}
+              color={colorScheme === "dark" ? "white" : "black"}
+            />
           ),
         }}
       />
@@ -41,7 +45,11 @@ export default function TabLayout() {
         options={{
           title: "Lista",
           tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="list.bullet" color={color} />
+            <MaterialIcons
+              name="storage"
+              size={28}
+              color={colorScheme === "dark" ? "white" : "black"}
+            />
           ),
         }}
       />
